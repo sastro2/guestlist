da7146a981f0fbcec1a3117f561789305c34abc7
Sascha Stropek <sascha.stropek@gmail.com>
2022-05-21 10:54:16 +0200

    Changed baseURL to deployed server

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { css } from '@emotion/react';
 import { useEffect, useRef, useState } from 'react';
 
-const baseURL = `http://localhost:4000/guests`;
+const baseURL = `https://guestlist-server.herokuapp.com/`;
 
 const baseStyle = css`
   display: flex;
